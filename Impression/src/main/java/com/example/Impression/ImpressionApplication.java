1cc4c3bdd4ee091ca7d1fdc132b7c1f38e00abd9
walid BEN-HAMID <walid.benhamid@axone-sys.com>
2025-06-30 02:16:57 +0100

    Initial commit: Projet Impression Spring Boot

---
+++ Impression/src/main/java/com/example/Impression/ImpressionApplication.java
@@ -0,0 +1,13 @@
+package com.example.Impression;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpressionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImpressionApplication.class, args);
+	}
+
+}
---
+++ Impression/src/test/java/com/example/Impression/ImpressionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Impression;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpressionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
